939764734abe4a95816c3c6e79c98ef3de35eec3
MubasharMurtza <mubashar222@gmail.com>
2023-08-02 18:01:04 +0500

    changing on search bar

--- src/Components/ScaleSoft/ScaleParty.js
+++ src/Components/ScaleSoft/ScaleParty.js
@@ -1,28 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import ScalePartyModal from './ScalePartyModal';
+import React, { useEffect, useState } from "react";
+import ScalePartyModal from "./ScalePartyModal";
 import Button from "react-bootstrap/Button";
-import axios from 'axios';
-import SearchBar from '../SearchBar';
-
+import axios from "axios";
+import SearchBar from "../SearchBar";
 
 export default function ScaleParty() {
-  const [showModal, setShowModal] = useState(false)
-  
-  const [list, setList] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const [data, setdata] = useState([]);
   useEffect(() => {
-    axios.get('https://localhost:44327/api/scaleparty')
-        .then(res => {setList(res.data)})
-  }, [])
+    axios.get("https://localhost:44327/api/scaleparty").then((res) => {
+      setdata(res.data);
+    });
+  }, []);
 
   return (
     <>
-      <SearchBar allData = {list} />
-      <Button onClick={() => setShowModal(true)}>
-        Show Modal
-      </Button>
       <div>
+        <Button onClick={() => setShowModal(true)}>Show Modal</Button>
         <ScalePartyModal show={showModal} onHide={() => setShowModal(false)} />
       </div>
+      <SearchBar list={data} />
     </>
-    )
+  );
 }
